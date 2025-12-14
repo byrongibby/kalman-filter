67d12868f315095c084fcfad08e899af066fe5e8
byron <byron@byrongibby.online>
2025-12-14 08:46:57 +0200

    Feature: Add flags to turn on/off sections of the KFAS

--- src/kfas0.c
+++ src/kfas0.c
@@ -1,6 +1,7 @@
 // Kalman Filter (univariate treatment)
 // Byron Botha 2025
 
+#include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -34,7 +35,10 @@ static int epssmooth(
     double *etahat,
     double *V_eta,
     double *alphahat,
-    double *V
+    double *V,
+    bool filter,
+    bool smoother,
+    bool disturbances
 ) {
   // Scalars required for BLAS/LAPACK routines
   char *tr = "t";
@@ -62,178 +66,187 @@ static int epssmooth(
   double *Ident = calloc(m * m, sizeof(double));
   for (int i = 0; i < m; i++) Ident[(i * m) + i] = 1.0;
 
-  // Initialise v = y
-  memcpy(v, y, n * p * sizeof(double));
-  
-  // Filter
-  for (int t = 0; t < n; t++) {
-    memcpy(att + (t * m), a + (t * m), m * sizeof(double));
-    memcpy(Ptt + (t * m * m), P + (t * m * m), m * m * sizeof(double));
+  if (filter) {
+    // Initialise v = y
+    memcpy(v, y, n * p * sizeof(double));
     
-    for (int i = 0; i < p; i++) {
-      // Innovation (6.13)
-      memcpy(F + (t * p) + i, H + (i * p) + i, sizeof(double));
-      F77_CALL(dgemv)(
-        nt, &inc, &m,
-        &neg, Z + i, &p, att + (t * m), &inc,
-        &pos, v + (t * p) + i, &inc, len_1);
-      F77_CALL(dgemm)(
-        nt, nt, &inc, &m, &m,
-        &pos, Z + i, &p, Ptt + (t * m * m), &m,
-        &zero, m1, &m, len_1, len_2);
-      F77_CALL(dgemv)(
-        nt, &inc, &m,
-        &pos, m1, &inc, Z + i, &p,
-        &pos, F + (t * p) + i, &inc, len_1);
+    // Filter
+    for (int t = 0; t < n; t++) {
+      memcpy(att + (t * m), a + (t * m), m * sizeof(double));
+      memcpy(Ptt + (t * m * m), P + (t * m * m), m * m * sizeof(double));
       
-      // K matrix (6.13)
-      Finv[(t * p) + i] = 1.0 / F[(t * p) + i];
+      for (int i = 0; i < p; i++) {
+        // Innovation (6.13)
+        memcpy(F + (t * p) + i, H + (i * p) + i, sizeof(double));
+        F77_CALL(dgemv)(
+          nt, &inc, &m,
+          &neg, Z + i, &p, att + (t * m), &inc,
+          &pos, v + (t * p) + i, &inc, len_1);
+        F77_CALL(dgemm)(
+          nt, nt, &inc, &m, &m,
+          &pos, Z + i, &p, Ptt + (t * m * m), &m,
+          &zero, m1, &m, len_1, len_2);
+        F77_CALL(dgemv)(
+          nt, &inc, &m,
+          &pos, m1, &inc, Z + i, &p,
+          &pos, F + (t * p) + i, &inc, len_1);
+        
+        // K matrix (6.13)
+        Finv[(t * p) + i] = 1.0 / F[(t * p) + i];
+        F77_CALL(dgemv)(
+          nt, &m, &m,
+          Finv + (t * p) + i, Ptt + (t * m * m), &m, Z + i, &p,
+          &zero, K + (t * m * p) + (i * m), &inc, len_1);
+
+        // Update state (6.12)
+        F77_CALL(dgemv)(
+          nt, &m, &inc,
+          &pos, K + (t * m * p) + (i * m), &p, v + (t * p) + i, &inc,
+          &pos, att + (t * m), &inc, len_1);
+        c = neg * F[(t * p) + i];
+        F77_CALL(dger)(
+          &m, &m,
+          &c, K + (t * m * p) + (i * m), &inc, K + (t * m * p) + (i * m), &inc,
+          Ptt + (t * m * m), &m);
+      }
+
+      // Predict state (6.14)
       F77_CALL(dgemv)(
         nt, &m, &m,
-        Finv + (t * p) + i, Ptt + (t * m * m), &m, Z + i, &p,
-        &zero, K + (t * m * p) + (i * m), &inc, len_1);
-
-      // Update state (6.12)
-      F77_CALL(dgemv)(
-        nt, &m, &inc,
-        &pos, K + (t * m * p) + (i * m), &p, v + (t * p) + i, &inc,
-        &pos, att + (t * m), &inc, len_1);
-      c = neg * F[(t * p) + i];
-      F77_CALL(dger)(
-        &m, &m,
-        &c, K + (t * m * p) + (i * m), &inc, K + (t * m * p) + (i * m), &inc,
-        Ptt + (t * m * m), &m);
+        &pos, T, &m, att + (t * m), &inc,
+        &zero, a + ((t + 1) * m), &inc, len_1);
+      F77_CALL(dgemm)(
+        nt, nt, &m, &q, &q,
+        &pos, R, &m, Q, &q,
+        &zero, mq, &m, len_1, len_2);
+      F77_CALL(dgemm)(
+        nt, tr, &m, &m, &q,
+        &pos, mq, &m, R, &q,
+        &zero, P + ((t + 1) * m * m), &m, len_1, len_2);
+      F77_CALL(dgemm)(
+        nt, nt, &m, &m, &m,
+        &pos, T, &m, Ptt + (t * m * m), &m,
+        &zero, mm, &m, len_1, len_2);
+      F77_CALL(dgemm)(
+        nt, tr, &m, &m, &m,
+        &pos, mm, &m, T, &m,
+        &pos, P + ((t + 1) * m * m), &m, len_1, len_2);
     }
-
-    // Predict state (6.14)
-    F77_CALL(dgemv)(
-      nt, &m, &m,
-      &pos, T, &m, att + (t * m), &inc,
-      &zero, a + ((t + 1) * m), &inc, len_1);
-    F77_CALL(dgemm)(
-      nt, nt, &m, &q, &q,
-      &pos, R, &m, Q, &q,
-      &zero, mq, &m, len_1, len_2);
-    F77_CALL(dgemm)(
-      nt, tr, &m, &m, &q,
-      &pos, mq, &m, R, &q,
-      &zero, P + ((t + 1) * m * m), &m, len_1, len_2);
-    F77_CALL(dgemm)(
-      nt, nt, &m, &m, &m,
-      &pos, T, &m, Ptt + (t * m * m), &m,
-      &zero, mm, &m, len_1, len_2);
-    F77_CALL(dgemm)(
-      nt, tr, &m, &m, &m,
-      &pos, mm, &m, T, &m,
-      &pos, P + ((t + 1) * m * m), &m, len_1, len_2);
   }
 
-  // Initialise alphahat = a, and V = P
-  memcpy(alphahat, a, n * m * sizeof(double));
-  memcpy(V, P, n * m * m * sizeof(double));
+  if (smoother || disturbances) {
+    // Initialise alphahat = a, and V = P
+    memcpy(alphahat, a, n * m * sizeof(double));
+    memcpy(V, P, n * m * m * sizeof(double));
 
-  // Initialise epshat = v, and V_eps = F
-  memcpy(epshat, v, n * p * sizeof(double));
-  memcpy(V_eps, Finv, n * p * sizeof(double));
+    // Initialise epshat = v, and V_eps = F
+    memcpy(epshat, v, n * p * sizeof(double));
+    memcpy(V_eps, Finv, n * p * sizeof(double));
 
-  // Smooth
-  for (int t = n - 1; t >= 0; t--) {
-
-    // Previous weighted sum of innovations (6.15.2)
-    F77_CALL(dgemv)(
-        tr, &m, &m,
-        &pos, T, &m, r + ((t + 1) * m), &inc,
-        &zero, r + (t * m), &inc, len_1);
-    F77_CALL(dgemm)(
-        tr, nt, &m, &m, &m,
-        &pos, T, &m, N + ((t + 1) * m * m), &m,
-        &zero, mm, &m, len_1, len_2);
-    F77_CALL(dgemm)(
-        nt, nt, &m, &m, &m,
-        &pos, mm, &m, T, &m,
-        &zero, N + (t * m * m), &m, len_1, len_2);
-
-    // Smoothed eta disturbances (4.69)
-    memcpy(V_eta + (t * q * q), Q, q * q * sizeof(double));
-    F77_CALL(dgemm)(
-      nt, tr, &q, &m, &q,
-      &pos, Q, &q, R, &m,
-      &zero, qm_1, &q, len_1, len_2);
-    F77_CALL(dgemv)(
-      nt, &q, &m,
-      &pos, qm_1, &q, r + ((t + 1) * m), &inc,
-      &pos, etahat + (t * q), &inc, len_1);
-    F77_CALL(dgemm)(
-      nt, nt, &q, &m, &m,
-      &pos, qm_1, &q, N + ((t + 1) * m * m), &m,
-      &zero, qm_2, &q, len_1, len_2);
-    F77_CALL(dgemm)(
-      nt, tr, &q, &q, &m,
-      &neg, qm_2, &q, qm_1, &q,
-      &pos, V_eta + (t * q * q), &q, len_1, len_2);
-
-    for (int i = p - 1; i >= 0; i--) {
-      // Smoothed epsilon disturbances
+    // Smooth
+    for (int t = n - 1; t >= 0; t--) {
+      // Previous weighted sum of innovations (6.15.2)
       F77_CALL(dgemv)(
-        tr, &m, &inc,
-        &neg, K + (t * m * p) + (i * m), &m, r + (t * m), &inc,
-        Finv + (t * p) + i, epshat + (t * p) + i, &inc, len_1);
-      epshat[(t * p) + i] *= H[(i * p) + i];
-      F77_CALL(dgemv)(
-        nt, &m, &m,
-        &pos, N + (t * m * m), &m, K + (t * m * p) + (i * m), &inc,
-        &zero, m1, &inc, len_1);
-      F77_CALL(dgemv)(
-        tr, &m, &inc,
-        &pos, K + (t * m * p) + (i * m), &m, m1, &inc,
-        &pos, V_eps + (t * p) + i, &inc, len_1);
-      V_eps[(t * p) + i] *= H[(i * p) + i] * H[(i * p) + i];
-      V_eps[(t * p) + i] = H[(i * p) + i] - V_eps[(t * p) + i];
-
-      // L matrix
-      memcpy(L, Ident, m * m * sizeof(double));
+          tr, &m, &m,
+          &pos, T, &m, r + ((t + 1) * m), &inc,
+          &zero, r + (t * m), &inc, len_1);
       F77_CALL(dgemm)(
-        nt, nt, &m, &m, &inc,
-        &neg, K + (t * m * p) + (i * m), &m, Z + i, &p,
-        &pos, L, &m, len_1, len_2);
+          tr, nt, &m, &m, &m,
+          &pos, T, &m, N + ((t + 1) * m * m), &m,
+          &zero, mm, &m, len_1, len_2);
+      F77_CALL(dgemm)(
+          nt, nt, &m, &m, &m,
+          &pos, mm, &m, T, &m,
+          &zero, N + (t * m * m), &m, len_1, len_2);
 
-      // Weighted sum of innovations (6.15.1)
-      memcpy(m1, r + (t * m), m * sizeof(double));
-      F77_CALL(dgemv)(
-        tr, &m, &m,
-        &pos, L, &m, m1, &inc,
-        &zero, r + (t * m), &inc, len_1);
-      F77_CALL(dgemv)(
-        tr, &inc, &m,
-        Finv + (t * p) + i, Z + i, &p, v + (t * p) + i, &inc,
-        &pos, r + (t * m), &inc, len_1);
-      F77_CALL(dgemm)(
-        tr, nt, &m, &m, &m,
-        &pos, L, &m, N + (t * m * m), &m,
-        &zero, mm, &m, len_1, len_2);
-      F77_CALL(dgemm)(
-        nt, nt, &m, &m, &m,
-        &pos, mm, &m, L, &m,
-        &zero, N + (t * m * m), &m, len_1, len_2);
-      F77_CALL(dgemm)(
-        tr, nt, &m, &m, &inc,
-        Finv + (t * p) + i, Z + i, &p, Z + i, &p,
-        &pos, N + (t * m * m), &m, len_1, len_2);
+      if (disturbances) {
+        // Smoothed eta disturbances (4.69)
+        memcpy(V_eta + (t * q * q), Q, q * q * sizeof(double));
+        F77_CALL(dgemm)(
+          nt, tr, &q, &m, &q,
+          &pos, Q, &q, R, &m,
+          &zero, qm_1, &q, len_1, len_2);
+        F77_CALL(dgemv)(
+          nt, &q, &m,
+          &pos, qm_1, &q, r + ((t + 1) * m), &inc,
+          &pos, etahat + (t * q), &inc, len_1);
+        F77_CALL(dgemm)(
+          nt, nt, &q, &m, &m,
+          &pos, qm_1, &q, N + ((t + 1) * m * m), &m,
+          &zero, qm_2, &q, len_1, len_2);
+        F77_CALL(dgemm)(
+          nt, tr, &q, &q, &m,
+          &neg, qm_2, &q, qm_1, &q,
+          &pos, V_eta + (t * q * q), &q, len_1, len_2);
+      }
+
+      for (int i = p - 1; i >= 0; i--) {
+        if (disturbances) {
+          // Smoothed epsilon disturbances
+          F77_CALL(dgemv)(
+            tr, &m, &inc,
+            &neg, K + (t * m * p) + (i * m), &m, r + (t * m), &inc,
+            Finv + (t * p) + i, epshat + (t * p) + i, &inc, len_1);
+          epshat[(t * p) + i] *= H[(i * p) + i];
+          F77_CALL(dgemv)(
+            nt, &m, &m,
+            &pos, N + (t * m * m), &m, K + (t * m * p) + (i * m), &inc,
+            &zero, m1, &inc, len_1);
+          F77_CALL(dgemv)(
+            tr, &m, &inc,
+            &pos, K + (t * m * p) + (i * m), &m, m1, &inc,
+            &pos, V_eps + (t * p) + i, &inc, len_1);
+          V_eps[(t * p) + i] *= H[(i * p) + i] * H[(i * p) + i];
+          V_eps[(t * p) + i] = H[(i * p) + i] - V_eps[(t * p) + i];
+        }
+
+        // L matrix
+        memcpy(L, Ident, m * m * sizeof(double));
+        F77_CALL(dgemm)(
+          nt, nt, &m, &m, &inc,
+          &neg, K + (t * m * p) + (i * m), &m, Z + i, &p,
+          &pos, L, &m, len_1, len_2);
+
+        // Weighted sum of innovations (6.15.1)
+        memcpy(m1, r + (t * m), m * sizeof(double));
+        F77_CALL(dgemv)(
+          tr, &m, &m,
+          &pos, L, &m, m1, &inc,
+          &zero, r + (t * m), &inc, len_1);
+        F77_CALL(dgemv)(
+          tr, &inc, &m,
+          Finv + (t * p) + i, Z + i, &p, v + (t * p) + i, &inc,
+          &pos, r + (t * m), &inc, len_1);
+        F77_CALL(dgemm)(
+          tr, nt, &m, &m, &m,
+          &pos, L, &m, N + (t * m * m), &m,
+          &zero, mm, &m, len_1, len_2);
+        F77_CALL(dgemm)(
+          nt, nt, &m, &m, &m,
+          &pos, mm, &m, L, &m,
+          &zero, N + (t * m * m), &m, len_1, len_2);
+        F77_CALL(dgemm)(
+          tr, nt, &m, &m, &inc,
+          Finv + (t * p) + i, Z + i, &p, Z + i, &p,
+          &pos, N + (t * m * m), &m, len_1, len_2);
+      }
+
+      if (smoother) {
+        // Smoothed state (4.44)
+        F77_CALL(dgemv)(
+          nt, &m, &m,
+          &pos, P + (t * m * m), &m, r + (t * m), &inc,
+          &pos, alphahat + (t * m), &inc, len_1);
+        F77_CALL(dgemm)(
+          nt, nt, &m, &m, &m,
+          &pos, P + (t * m * m), &m, N + (t * m * m), &m,
+          &zero, mm, &m, len_1, len_2);
+        F77_CALL(dgemm)(
+          nt, nt, &m, &m, &m,
+          &neg, mm, &m, P + (t * m * m), &m,
+          &pos, V + (t * m * m), &m, len_1, len_2);
+      }
     }
-
-    // Smoothed state (4.44)
-    F77_CALL(dgemv)(
-      nt, &m, &m,
-      &pos, P + (t * m * m), &m, r + (t * m), &inc,
-      &pos, alphahat + (t * m), &inc, len_1);
-    F77_CALL(dgemm)(
-      nt, nt, &m, &m, &m,
-      &pos, P + (t * m * m), &m, N + (t * m * m), &m,
-      &zero, mm, &m, len_1, len_2);
-    F77_CALL(dgemm)(
-      nt, nt, &m, &m, &m,
-      &neg, mm, &m, P + (t * m * m), &m,
-      &pos, V + (t * m * m), &m, len_1, len_2);
   }
 
   free(Ident);
@@ -342,7 +355,10 @@ SEXP kfas0(
     etahat_ptr,
     V_eta_ptr,
     alphahat_ptr,
-    V_ptr
+    V_ptr,
+    true,
+    true,
+    true
   ) != 0) printf("Filter failed!\n");
 
   SET_VECTOR_ELT(result,  0, v);
